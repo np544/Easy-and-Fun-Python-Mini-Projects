6c9c1a58626d64348d186381fb78e1dcaa2b66b7
ParvathiJNair <ParvathiJNair@users.noreply.github.com>
2021-06-02 15:11:43 +0530

    rock,paper,scissors

---
+++ Rock Paper Scissors.py
@@ -0,0 +1,33 @@
+from random import randint
+
+while True:
+    choice=['rock','paper','scissors']
+    computer=choice[randint(0,2)]
+    player=input("Hi! Let's play Rock, Paper, Scissors!!\n To begin choose Rock, Paper or Scissor ").lower()
+    print("The computer's choice is "+computer)
+    if(player=='rock'):
+        if(computer=='paper'):
+            print('Computer is the winner!!')
+        elif(computer=='rock'):
+            print("Same input! Draw")
+        else:
+            print('You win!')
+    elif(player=='paper'):
+        if(computer=='scissor'):
+            print('Computer is the winner!!')
+        elif(computer=='paper'):
+            print("Same input! Draw")
+        else:
+            print('You win!')
+    elif(player=='scissor'):
+        if(computer=='rock'):
+            print('Computer is the winner!!')
+        elif(computer=='scissor'):
+            print("Same input! Draw")
+        else:
+            print('You win!')
+    else:
+        print("Invalid entry!! Check your input please.")
+    ch=input('Do you want to play again?(Y/N)').lower()
+    if(ch=='n'):
+        break;
