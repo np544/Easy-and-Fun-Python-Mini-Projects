21aa4f79986540a63b39b701027f32b8a6990af3
ParvathiJNair <ParvathiJNair@users.noreply.github.com>
2021-06-02 15:34:54 +0530

    score added

--- Rock Paper Scissors.py
+++ Rock Paper Scissors.py
@@ -1,6 +1,7 @@
 from random import randint
 
 while True:
+    score=0
     choice=['rock','paper','scissors']
     computer=choice[randint(0,2)]
     player=input("Hi! Let's play Rock, Paper, Scissors!!\n To begin choose Rock, Paper or Scissor ").lower()
@@ -12,6 +13,7 @@ while True:
             print("Same input! Draw")
         else:
             print('You win!')
+            score+=1
     elif(player=='paper'):
         if(computer=='scissor'):
             print('Computer is the winner!!')
@@ -19,6 +21,7 @@ while True:
             print("Same input! Draw")
         else:
             print('You win!')
+            score+=1
     elif(player=='scissor'):
         if(computer=='rock'):
             print('Computer is the winner!!')
@@ -26,8 +29,10 @@ while True:
             print("Same input! Draw")
         else:
             print('You win!')
+            score+=1
     else:
         print("Invalid entry!! Check your input please.")
     ch=input('Do you want to play again?(Y/N)').lower()
     if(ch=='n'):
-        break;
+        print(f'It was fun playing with you!! Your score is {score}')
+        break
